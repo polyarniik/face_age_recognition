d46f757720c45524b90d0e284033127f44e505d9
Ruslan Safiullin <rus.safiullin.01@mail.ru>
2021-05-26 19:37:29 +0300

    + models

--- recognition.py
+++ recognition.py
@@ -1,11 +1,17 @@
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+import tensorflow.keras.layers as L
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+
+
 
 data = pd.read_csv(r'dataset/age_gender.csv')
 
 data['pixels'] = data['pixels'].apply(lambda x: np.array(x.split(), dtype="float32"))
-
-data.head()
 
 print('Total rows: {}'.format(len(data)))
 print('Total columns: {}'.format(len(data.columns)))
@@ -32,5 +38,100 @@ gender_dist = data['gender'].value_counts().rename(index={0: 'Male', 1: 'Female'
 X = np.array(data['pixels'].tolist())
 
 ## Converting pixels from 1D to 3D
-print(X.shape)
 X = X.reshape(X.shape[0], 48, 48, 1)
+
+y = data['gender']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.22, random_state=37
+)
+
+model = tf.keras.Sequential([
+    L.InputLayer(input_shape=(48,48,1)),
+    L.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    L.BatchNormalization(),
+    L.MaxPooling2D((2, 2)),
+    L.Conv2D(64, (3, 3), activation='relu'),
+    L.MaxPooling2D((2, 2)),
+    L.Flatten(),
+    L.Dense(64, activation='relu'),
+    L.Dropout(rate=0.5),
+    L.Dense(1, activation='sigmoid')
+])
+
+
+model.compile(optimizer='sgd',
+              loss=tf.keras.losses.BinaryCrossentropy(),
+              metrics=['accuracy'])
+
+
+## Stop training when validation loss reach 0.2700
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('val_loss') < 0.2700):
+            print("\nReached 0.2700 val_loss so cancelling training!")
+            self.model.stop_training = True
+
+
+callback = myCallback()
+
+print(model.summary())
+
+history = model.fit(
+    X_train, y_train, epochs=20, validation_split=0.1, batch_size=64, callbacks=[callback]
+)
+
+loss, acc = model.evaluate(X_test,y_test,verbose=0)
+print('Test loss: {}'.format(loss))
+print('Test Accuracy: {}'.format(acc))
+
+
+y = data['age']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.22, random_state=37
+)
+
+model = tf.keras.Sequential([
+    L.InputLayer(input_shape=(48, 48, 1)),
+    L.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    L.BatchNormalization(),
+    L.MaxPooling2D((2, 2)),
+    L.Conv2D(64, (3, 3), activation='relu'),
+    L.MaxPooling2D((2, 2)),
+    L.Conv2D(128, (3, 3), activation='relu'),
+    L.MaxPooling2D((2, 2)),
+    L.Flatten(),
+    L.Dense(64, activation='relu'),
+    L.Dropout(rate=0.5),
+    L.Dense(1, activation='relu')
+])
+
+sgd = tf.keras.optimizers.SGD(momentum=0.9)
+
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['mae'])
+
+
+## Stop training when validation loss reach 110
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('val_loss') < 110):
+            print("\nReached 110 val_loss so cancelling training!")
+            self.model.stop_training = True
+
+
+callback = myCallback()
+
+print(model.summary())
+
+
+
+history = model.fit(
+    X_train, y_train, epochs=20, validation_split=0.1, batch_size=64, callbacks=[callback]
+)
+
+mse, mae = model.evaluate(X_test,y_test,verbose=0)
+print('Test Mean squared error: {}'.format(mse))
+print('Test Mean absolute error: {}'.format(mae))
